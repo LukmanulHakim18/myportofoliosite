ad93244a75ded4814033205996a6ccb857844509
LukmanulHakim18 <lukman.virg@gmail.com>
2020-02-15 22:28:56 +0700

    Updates

---
+++ precache-manifest.72019cdbefa13c9fd995fbe1ec2f9aae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b1e02d43430655d08864a80f329c2f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aeff3ac8d1451b5617db",
+    "url": "/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "b254afdea067117fd04d",
+    "url": "/static/css/main.6a3a4fdd.chunk.css"
+  },
+  {
+    "revision": "aeff3ac8d1451b5617db",
+    "url": "/static/js/2.16a43d0b.chunk.js"
+  },
+  {
+    "revision": "b254afdea067117fd04d",
+    "url": "/static/js/main.894260b1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "692e2f41091019eac485e431cbae8037",
+    "url": "/static/media/me.692e2f41.jpg"
+  }
+]);
